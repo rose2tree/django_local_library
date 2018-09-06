4d4db76dd442b0696c271d0ecd69f683e8cd61c0
rose2tree <rose2tree@gmail.com>
2018-09-06 21:51:49 +0800

    First version of application moved into github

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l$-po2xyri9e_*c!@hvr%$vrnu%*ix_bl-262bswc6q+wzi744'
+#SECRET_KEY = 'l$-po2xyri9e_*c!@hvr%$vrnu%*ix_bl-262bswc6q+wzi744'
+
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
